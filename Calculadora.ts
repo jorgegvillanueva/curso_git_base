9ce0c18e7fd9a12cf8f033ce11b2da5da34be4d3
Jorge <jorgegvillanueva@gmail.com>
2023-05-17 19:17:22 +0200

    añadida resta

--- Calculadora.ts
+++ Calculadora.ts
@@ -4,8 +4,13 @@ class Calculadora {
     memo: number = 0;
     tempValue: number = 0;
 
-    add(value: number): number {
+    suma(value: number): number {
         this.tempValue += value;
+        return this.tempValue;
+    }
+
+    resta(value: number): number {
+        this.tempValue -= value;
         return this.tempValue;
     }
 

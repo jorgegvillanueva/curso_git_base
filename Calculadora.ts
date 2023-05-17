6552a6f140a28488086058caf2f3d6bb74d26b9d
Jorge <jorgegvillanueva@gmail.com>
2023-05-17 19:20:43 +0200

    refactorizadas las funciones suma y resta

--- Calculadora.ts
+++ Calculadora.ts
@@ -4,14 +4,14 @@ class Calculadora {
     memo: number = 0;
     tempValue: number = 0;
 
-    suma(value: number): number {
+    suma(value: number): string {
         this.tempValue += value;
-        return this.tempValue;
+        return this.show();
     }
 
-    resta(value: number): number {
+    resta(value: number): string {
         this.tempValue -= value;
-        return this.tempValue;
+        return this.show();
     }
 
     show(): string {
